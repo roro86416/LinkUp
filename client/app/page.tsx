55b267981365814568f0db8aa7a6cf3c4a3e8f0c
Verdux1337 <jackychang8412@gmail.com>
2025-11-02 00:59:05 +0800

    整合並遷移所有模組的資料庫

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    
+    // 至少要有一個最外層的標籤，例如 <div> 或 React Fragment (<>...</>)
+    <div>
+      <h1>LinkUp! 專案首頁</h1>
+      <p>
+        這裡是 /client/app/page.tsx
+      </p>
+      <p>
+        模組二 (活動發布) 開發中...
+      </p>
+    </div>
   );
-}
+}   
