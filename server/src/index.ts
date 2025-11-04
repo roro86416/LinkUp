2f298bf30d3a104238e89db8d9b3394b2eb658ad
roro86416 <g650azy@gmail.com>
2025-11-04 10:27:46 +0800

    更新：app.ts & index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,11 @@
-//server/src/index.ts
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import organizerRoutes from "./api/api-organizer";
+import app from "./app";
 
 // --- 伺服器設定 ---
-dotenv.config();
-const app: Express = express();
 const port = process.env.PORT || 3001;
 
 // --- 中間件 (Middlewares) ---
 app.use(cors());
-app.use(express.json()); 
-
-// --- API 路由註冊 (Routes) ---
-
-// 1. 測試路由
-app.get("/api/test", (req: Request, res: Response) => {
-  res.json({ message: "愛來自LinkUp伺服器! 🚀" });
-});
-
-// 2. 模組三 (產品) 路由 
-// app.use("/api/v1/products", productRoutes);
-
-// 3. 模組二 (主辦方) 路由
-app.use("/api/v1/organizer", organizerRoutes);
+app.use(express.json());
 
 // --- 啟動伺服器 ---
 app.listen(port, () => {
